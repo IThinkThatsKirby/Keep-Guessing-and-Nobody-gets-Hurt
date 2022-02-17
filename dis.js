62f9f2a5683661db917d73de05918cdb7a73bc69
Kirby Ellingson <hkellingson@gmail.com>
2022-02-16 20:40:12 -0600

    created DIVS with ID that corraltes to words

--- dis.js
+++ dis.js
@@ -1,30 +1,31 @@
 // get words from api https://random-word-api.herokuapp.com/home DONE
 // parse dat data DONE
-// pick random letter from word
-// place random letter at 10 second interval
+// make div with ID match to words
 //
 let usedWords = [];
 let word = []
-
 // get random words
 
 async function getRandomWords(){
-    let response = await fetch('https://random-word-api.herokuapp.com/word?number=10&swear=0')
-    let data = await response.json()
-    // console.log(data)
-    //word.push([...data])
-    return data
+    return fetch('https://random-word-api.herokuapp.com/word?number=10&swear=0')
+            .then(res => res.json())
 }
-// getRandomWords()
-// setTimeout(()=>console.log(word),2000)
-// async function pickRandomWord(){
-//     usedWords.push  = await getRandomWords()
+// create div of word to type
+function displayWord(words, index){
+    text = document.createElement('p')
+    text.innerHTML = words
+    text.id = index
+    document.getElementById('words').appendChild(text)
+}
 
-// }
-// put a random word in word array
 window.onload = async () =>{
 word = await getRandomWords()
+console.log(word)
+// const words = document.createElement('p');
+// words.innerHTML = "this is it";
+// document.getElementById('words').appendChild(words);
 
+word.forEach(displayWord)
 }
 
 // do thing.
