4d6ee0b00eb1aab24d1b05ab36a57398934135ed
Kirby Ellingson <hkellingson@gmail.com>
2022-02-16 21:49:16 -0600

    create answer input feilds for each word

--- dis.js
+++ dis.js
@@ -1,31 +1,43 @@
 // get words from api https://random-word-api.herokuapp.com/home DONE
 // parse dat data DONE
-// make div with ID match to words
-//
-let usedWords = [];
+// make div with ID match to words DONE
+//make answer space
+//compare answer array with word array
 let word = []
+let answer = []
 // get random words
 
 async function getRandomWords(){
     return fetch('https://random-word-api.herokuapp.com/word?number=10&swear=0')
             .then(res => res.json())
 }
-// create div of word to type
+// create div of word to type and give ID
 function displayWord(words, index){
     text = document.createElement('p')
     text.innerHTML = words
-    text.id = index
+    text.id = 'words'+index
     document.getElementById('words').appendChild(text)
 }
-
+// create input fields of answers and give ID based on index
+function playerAnswer(answers, index){
+    text = document.createElement('input')
+    text.innerHTML = answers
+    text.id = 'answer'+index
+    text.placeholder = 'Answer'+ ' ' + index
+    document.getElementById('answers').appendChild(text)
+}
+// function wrapper since await isnt a global layer thing yet :(
 window.onload = async () =>{
 word = await getRandomWords()
-console.log(word)
+//console.log(word)
 // const words = document.createElement('p');
 // words.innerHTML = "this is it";
 // document.getElementById('words').appendChild(words);
 
 word.forEach(displayWord)
+word.forEach(playerAnswer)
 }
 
-// do thing.
+if (document.getElementsByTagName('input').innerHTML === document.getElementsByTagName('text').value
+){
+console.log('yay')}
