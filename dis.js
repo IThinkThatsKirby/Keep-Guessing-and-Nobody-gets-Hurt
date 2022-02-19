e1c8e13e70e40270d84349786ebbfa3bfff5a2e6
Kirby Ellingson <hkellingson@gmail.com>
2022-02-19 17:51:09 -0600

    added compare for all user answers as they tab out

--- dis.js
+++ dis.js
@@ -1,12 +1,14 @@
 // get words from api https://random-word-api.herokuapp.com/home DONE
 // parse dat data DONE
 // make div with ID match to words DONE
-//make answer space
-//compare answer array with word array
+// make answer space DONE
+// compare answer array with word array DONEish
+// make score counter go up
 let word = []
 let answer = []
 let form0 = []
 let form1 = []
+let i = 0;
 
 // get random words
 
@@ -28,31 +30,42 @@ function playerAnswer(array,index){
     text.placeholder = 'Answer'+ ' ' + index
     document.getElementById('answers').appendChild(text)
 }
-// compare answer input.value with displayeWord.
-// function compareAnswer(userInput,correctWord,index){
-//     if (userInput.value === displayedWord) {
-//             document.getElementById('answer'+index).style.backgroundColor = 'green'
-//         }
-    
-// }
-// function wrapper since await isnt a global layer thing yet :(
 // compare
 function compare(){
     if (form1.value == form0.innerText) {
         form1.style.backgroundColor = 'green'
+        console.log(i)
+        updateForm()
     } else {
         form1.style.backgroundColor = 'red'
+        console.log(i)
+        updateForm()
     }
 }
-window.onload = async () =>{
-word = await getRandomWords()
-
-word.forEach(displayWord)
-word.forEach(playerAnswer)
-form0 = document.getElementById('words'+0)
-form1 = document.getElementById('answer'+0)
-console.log(form0.innerText)
-form1.addEventListener('focusout',async () => {
-    await compare()
-})
+// update i
+function iUpdate(){
+    if (i < word.length - 1) {i++} else {return i = 0} // prevents error throw on tabout of last input
 }
+//update forms 0 and 1
+async function updateForm(){
+    await iUpdate()
+    console.log(i)
+    form0 = document.getElementById('words'+i)
+    form1 = document.getElementById('answer'+i)
+    console.log(form0)
+    form1.addEventListener('focusout', async () => await compare())
+}
+// function wrapper since await isnt a global layer thing yet :(
+window.onload = async () =>{
+    word = await getRandomWords()
+    
+    word.forEach(displayWord)
+    word.forEach(playerAnswer)
+    form0 = document.getElementById('words'+i)
+    form1 = document.getElementById('answer'+i)
+    console.log(form0.innerText)
+    console.log(form1)
+    console.log(i)
+    form1.addEventListener('focusout', async () => await compare())
+}
+
