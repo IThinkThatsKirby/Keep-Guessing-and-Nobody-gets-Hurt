d8e6350b4e6a108b4deee1048458d943fab5593b
Kirby Ellingson <hkellingson@gmail.com>
2022-02-23 12:35:15 -0600

    "QoL" updates and new round tab loop correctly.

--- dis.js
+++ dis.js
@@ -35,8 +35,12 @@ function playerAnswer(array,index){
     text = document.createElement('input')
     text.id = 'answer'+index
     text.placeholder = 'Answer'+ ' ' + index
+    text.setAttribute('data-id', 'updatable')
     text.classList.add('form-control')
+    text.setAttribute('tabindex', index+1)
+    text.setAttribute('autocomplete', 'off')
     document.getElementById('answers').appendChild(text)
+    document.getElementById('answer0').autofocus
     
 }
 
@@ -45,20 +49,16 @@ function playerAnswer(array,index){
 function compare(){
     if (form1.value == form0.innerText) {
         form1.style.backgroundColor = 'green'
-        console.log(i)
+        form1.readOnly = true
+        // score stuff
         updateForm()
     } else {
         form1.style.backgroundColor = 'red'
-        console.log(i)
+        form1.readOnly = true
+        // score stuff
         updateForm()
     }
-}
-
-// update i
-
-//function iUpdate(){
- // prevents error throw on focusout of last input
-//}
+}       
 
 //update forms 0 and 1
 
@@ -67,47 +67,38 @@ function updateForm(){
         i++
         form0 = document.getElementById('words'+i)
         form1 = document.getElementById('answer'+i)
-        console.log(form0)
+        //
         form1.addEventListener('focusout', compare)
     } else if (i = word.length) {
         i = 0
-        newRound();
+        setTimeout(newRound, 500)
     }
-    console.log(i)
+    //
 }
 
 // function wrapper since await isnt a global layer thing yet :(
 
 // new round function
-
 async function newRound(){
-    document.getElementById("answers0").focus();
+    
     elements = document.querySelectorAll(("[data-id='updatable']"))
-    console.log(elements)
     elements.forEach((el)=>{el.remove()})
     word = await getRandomWords()
-    word.forEach(displayWord)
-    form0 = document.getElementById('words'+i)
-    form1 = document.getElementById('answer'+i)
-    
-    form1.addEventListener('focusout', compare)
-    // text.id = 'words'+index
-    // document.getElementById('words').appendChild(text)
-}
-
-window.onload = async () =>{
-    word = await getRandomWords()
-    // let fakeWords = await getRandomWords()
-    // for (let i = 0; i < fakeWords.length; i++) {
-    //     allWords[`word${i}`] = fakeWords[i]
-    // }
-    //console.log(allWords)
     word.forEach(displayWord)
     word.forEach(playerAnswer)
     form0 = document.getElementById('words'+i)
     form1 = document.getElementById('answer'+i)
-    console.log(form0.innerText)
-    console.log(form1)
-    console.log(i)
+    let el = document.getElementById('answer0')
+    el.focus()
+    form1.addEventListener('focusout', compare)
+}
+
+window.onload = async () =>{
+    word = await getRandomWords()
+    word.forEach(displayWord)
+    word.forEach(playerAnswer)
+    form0 = document.getElementById('words'+i)
+    form1 = document.getElementById('answer'+i)
+    document.getElementById('answer0').focus()
     form1.addEventListener('focusout', compare)
 }
